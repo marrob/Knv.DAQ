7b7b8014ad7c954c27a8ec6c323ceef386378ef8
marrob <marrob50hz@gmail.com>
2024-02-18 21:02:12 +0100

    Update ADC configuration

--- IO/MPRT220416_ADC_FW_UnitTest.cs
+++ IO/MPRT220416_ADC_FW_UnitTest.cs
@@ -8,6 +8,7 @@ namespace Knv.DAQ.IO
     using System.IO;
     using System.IO.Ports;
     using System.Linq;
+    using System.Runtime.Remoting.Channels;
     using System.Text;
 
     [TestFixture]
@@ -70,10 +71,6 @@ namespace Knv.DAQ.IO
         [Test]
         public void AdcResultDownload_Block()
         {
-            /*
-             * MEMR <Address of Block> <Size of Block> 
-             * <Address of Block> <Size of Block> <Data> <crc>
-             */
             var sp = new SerialPort(COM_PORT, 460800, Parity.None, 8, StopBits.One);
             sp.Open();
             sp.NewLine = "\r";
@@ -82,39 +79,22 @@ namespace Knv.DAQ.IO
 
             var idn = Identification(sp);
             Assert.AreEqual("MPRT220416-ADC", idn);
-
             long timestamp = DateTime.Now.Ticks;
-            int adcSampleBufferSize = GetBufferSize(sp);
-            byte[] buffer = new byte[adcSampleBufferSize];
-            int block_size = GetMaxBlockSize(sp);
-            int blocks = adcSampleBufferSize / block_size;
-            int single_bytes = adcSampleBufferSize % block_size;
+
+
 
             AdcSoftwareTrigger(sp);
 
-            int block_address = 0;
-            int buffer_offset = 0;
-            for (int i = 0; i < blocks; i++)
-            {
-                var data = MemoryRead(sp, block_address, block_size);
-                Array.Copy(data, 0, buffer, buffer_offset, block_size);
-                block_address += block_size;
-                buffer_offset += block_size;
-            }
+            SetConfiguration(sp, customSamplesCount: 1024, adcDivider: 4);
 
-            var singles = MemoryRead(sp, block_address, single_bytes);
-            Array.Copy(singles, 0, buffer, buffer_offset, single_bytes);
+            var cfg = new AnalogInputGetConfiguration(sp);
+            UInt16[] valuesBuffer = BufferDownload(sp, cfg.BufferSize, cfg.BlockSize);
 
             Console.WriteLine($"ADC Data Download Time: {(DateTime.Now.Ticks - timestamp) / 10000} ms");
 
-            int samplesCount = buffer.Length / 2;
-
-            UInt16[] samplesBuffer = new UInt16[samplesCount];
-            for (int i = 0; i < samplesCount; i++)
-                samplesBuffer[i] = BitConverter.ToUInt16(buffer, i * 2);
 
             var lines = new List<string>();
-            samplesBuffer.ToList().ForEach(x => lines.Add(x.ToString()));
+            valuesBuffer.ToList().ForEach(x => lines.Add(x.ToString()));
             var dt = DateTime.Now;
             var fileName = $"MPRT220416_ADC_{dt:yyyy}{dt:MM}{dt:dd}_{dt:HH}{dt:mm}{dt:ss}.csv";
             using (var file = new StreamWriter($"{TestContext.CurrentContext.TestDirectory}\\{fileName}", true, Encoding.ASCII))
@@ -123,29 +103,43 @@ namespace Knv.DAQ.IO
             sp.Close();
         }
 
+        class AnalogInputGetConfiguration
+        {
+            public int CustomSamplesCount { get; private set; }
+            public int BufferSize { get; private set; }
+            public int AdcDivider { get; private set; }
+            public int BlockSize { get; private set; }
+
+            public AnalogInputGetConfiguration(SerialPort sp)
+            {
+                string cmd = $"AI:CFG?";
+                sp.WriteLine(cmd);
+                var response = sp.ReadLine();
+                var args = response.Split(' ');
+
+                CustomSamplesCount = int.Parse(args[0], NumberStyles.AllowHexSpecifier, CultureInfo.GetCultureInfo("en-US"));
+                BufferSize = int.Parse(args[1], NumberStyles.AllowHexSpecifier, CultureInfo.GetCultureInfo("en-US"));
+                AdcDivider = int.Parse(args[2], NumberStyles.AllowHexSpecifier, CultureInfo.GetCultureInfo("en-US"));
+                BlockSize = int.Parse(args[3], NumberStyles.AllowHexSpecifier, CultureInfo.GetCultureInfo("en-US"));
+            }
+        }
+
+
+        public void SetConfiguration(SerialPort sp, int customSamplesCount, int adcDivider)
+        {
+            sp.WriteLine($"AI:CFG {customSamplesCount:X4} {adcDivider:X2}");
+            var response = sp.ReadLine();
+            if (response != "OK")
+                throw new ApplicationException(response);
+        }
+     
+
 
         public string Identification(SerialPort sp)
         {
             sp.WriteLine("*IDN?");
             var response = sp.ReadLine();
             return response;
-        }
-
-        public int GetMaxBlockSize(SerialPort sp)
-        {
-            sp.WriteLine("AI:BSIZE?");
-            var response = sp.ReadLine();
-            int.TryParse(response, NumberStyles.HexNumber, CultureInfo.GetCultureInfo("en-US"), out int size);
-            return size;
-        }
-
-
-        public int GetBufferSize(SerialPort sp)
-        {
-            sp.WriteLine("AI:SIZE?");
-            var response = sp.ReadLine();
-            int.TryParse(response, NumberStyles.HexNumber, CultureInfo.GetCultureInfo("en-US"), out int size);
-            return size;
         }
 
 
@@ -166,7 +160,43 @@ namespace Knv.DAQ.IO
         }
 
 
-        public Byte[] MemoryRead(SerialPort sp, int address, int size)
+
+        public UInt16[] BufferDownload(SerialPort sp, int bufferSize, int blockSize)
+        {
+            byte[] buffer = new byte[bufferSize];
+            int blocks = bufferSize / blockSize;
+            int single_bytes = bufferSize % blockSize;
+
+            int blockAddress = 0;
+            int bufferOffset = 0;
+            for (int i = 0; i < blocks; i++)
+            {
+                var data = BufferRead(sp, blockAddress, blockSize);
+                Array.Copy(data, 0, buffer, bufferOffset, blockSize);
+                blockAddress += blockSize;
+                bufferOffset += blockSize;
+            }
+
+            if (single_bytes > 0)
+            {
+                var singles = BufferRead(sp, blockAddress, single_bytes);
+                Array.Copy(singles, 0, buffer, bufferOffset, single_bytes);
+            }
+
+
+            int samplesCount = buffer.Length / 2;
+
+            UInt16[] valuesBuffer = new UInt16[samplesCount];
+            for (int i = 0; i < samplesCount; i++)
+                valuesBuffer[i] = BitConverter.ToUInt16(buffer, i * 2);
+
+            return valuesBuffer;
+        }
+
+
+
+
+        public Byte[] BufferRead(SerialPort sp, int address, int size)
         {
             /*
              * MEMR <Address of Block> <Size of Block> 
